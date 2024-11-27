40ee9f352a39ab0f8eb66687e2ba10ec9d68b9a4
emreertugrl <emrertugrl@gmail.com>
2024-11-27 21:17:12 +0300

    Card component render to list component

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,27 @@
 import Buttons from "../../components/buttons";
+import Filter from "../../components/filter";
 import Hero from "../../components/hero";
+import List from "../../components/list";
 
 const Home = () => {
   return (
     <div>
       <Hero />
+
       <Buttons />
+
+      <h1 className="text-xl md:text-2xl lg:text-3xl xl:text-4xl font-semibold mb-4 md:mt-7 xl:mt-8">
+        Sana Uygun Seçenekler
+      </h1>
+
+      <div className="grid grid-cols-4 gap-5">
+        <div className="max-lg:hidden">
+          <Filter />
+        </div>
+        <div className="lg:col-span-3 col-span-4 ">
+          <List />
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/types.ts
@@ -0,0 +1,12 @@
+export type Shoe = {
+  id: string;
+  name: string;
+  picture: string[];
+  description: string;
+  isNew: boolean;
+  size: string;
+  color: string;
+  gender: string;
+  price: number;
+  discount?: number;
+};
