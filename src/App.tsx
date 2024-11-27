e3b20713773b7bcf4e492abbd5d28d266ddc03f7
emreertugrl <emrertugrl@gmail.com>
2024-11-27 12:59:07 +0300

    db.json, pages, components , routes installation was done.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/header";
+import Home from "./pages/home";
+import Detail from "./pages/detail/index";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <div className="min-h-screen bg-gray px-4 md:px-8 lg:px-12 xl:px-14 py-8">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/detail/:id" element={<Detail />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 };
 
 export default App;
